b092e6be0297db41cf1c95b23d51bbd3996099dd
zmajew <zmajew@gmail.com>
2019-01-04 19:15:08 +0100

    flags

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/neo4j/neo4j-go-driver/neo4j"
@@ -33,11 +35,11 @@ var (
 func hint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	session, err = driver.Session(neo4j.AccessModeWrite)
+	session, err = driver.Session(neo4j.AccessModeRead)
 	if err != nil {
-		log.Println("error quering database:", err)
+		log.Println("error 1 quering database:", err)
 		w.WriteHeader(500)
-		w.Write([]byte("error quering database:"))
+		w.Write([]byte("error 2 quering database:"))
 	}
 	defer session.Close()
 
@@ -46,13 +48,13 @@ func hint(w http.ResponseWriter, r *http.Request) {
 		"prTit": letters["lett"],
 	}
 
-	result, err = session.Run(`CALL db.index.fulltext.queryNodes("titles", $prTit) YIELD node, score
+	result, err = session.Run(`CALL db.index.fulltext.queryNodes("titles2", $prTit) YIELD node, score
 	RETURN node.primaryTitle, node.startYear, node.ratings, node.numberOfVotes, node.titleID, score LIMIT 7
 									`, mapa)
 	if err != nil {
-		log.Println("error quering database:", err)
+		log.Println("error 3 quering database:", err)
 		w.WriteHeader(500)
-		w.Write([]byte("error quering database:"))
+		w.Write([]byte("error 4 quering database:"))
 	}
 
 	res := []movieResult{}
@@ -76,7 +78,11 @@ func hint(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	driver, err = neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("user_name", "password", ""))
+	boltAdress := flag.String("bolt", "bolt://localhost:7687", "bolt adress of database (default: bolt://localhost:7687)")
+	port := flag.Int("port", 9090, "port number (default 9090)")
+	flag.Parse()
+
+	driver, err = neo4j.NewDriver(*boltAdress, neo4j.BasicAuth(string("neo4j"), string("trustno1"), ""))
 	if err != nil {
 		log.Fatalln("error connecting to database:", err)
 	}
@@ -85,5 +91,5 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/hint/{lett}", hint).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":9090", r))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), r))
 }
